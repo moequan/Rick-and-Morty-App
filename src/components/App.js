a2a8735693cc87a41353ec8f6099dc5cccdb1749
moequan <maurice.s93@web.de>
2020-01-22 11:12:01 +0100

    Starting Rick and Morty App

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "../scss/App.scss";
+
+function App() {
+  const [value,setValue] = useState("");
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
