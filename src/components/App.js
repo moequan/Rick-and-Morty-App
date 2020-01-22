2bad523dbd2e072fd999b2bb60c3a9f38e340a6e
moequan <maurice.s93@web.de>
2020-01-22 12:04:47 +0100

    First Hook (useEffect) and fetching data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, { useState, useEffect } from "react";
-import "../scss/App.scss";
+import React, { useState, useEffect } from 'react';
+import '../scss/App.scss';
 
-function App() {
-  const [value,setValue] = useState("");
-  return (
-    <div>
-      <h1>Rick and Morty</h1>
-    </div>
-  );
-}
+const App = () => {
+  const [episodes, setEpisodes] = useState({ info: {}, results: [] });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const url = `https://rickandmortyapi.com/api/episode`;
+    const data = await (await fetch(url)).json();
+    setEpisodes(data);
+  };
+
+  const episodesList = episodes.results.map(el => {
+    return <p>{el.name}</p>;
+  });
+
+  return <div className="App">{episodesList}</div>;
+};
 
 export default App;
